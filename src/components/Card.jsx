f78f269d310ec41f8892795c99923fac34f821d0
sourav2316 <ksourav044@gmail.com>
2024-07-03 13:58:50 +0530

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,7 +16,7 @@ const Card = ({ movie }) => {
       {isLoading ? (
         <div className="cards">
           <SkeletonTheme baseColor="#202020" highlightColor="#444">
-            <Skeleton height={300} duration={1} />
+            <Skeleton height={600} duration={1} />
           </SkeletonTheme>
         </div>
       ) : (
